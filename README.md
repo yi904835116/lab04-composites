a5e7bdc20ef36159f3a1aaf1cd5eea02809e8869
Luis Naranjo <luisnaranjo733@gmail.com>
2017-01-23 13:26:23 -0800

    initial readme

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# lab04-composites
+# Composites Lab
+
+## What is a composite?
+
+Put high level conceptual introduction to what a composite is.
+Give 1-2 links to really good explanations out there by other people.
+
+OR
+
+delete this section and give a mini lecture in person.
+
+## Your task
+
+include some HTML DOM review content here
+
+Your task for this lab is to write TypeScript that constructs and prints out an HTML DOM using the composites pattern.
+
+Your output will look something like this:
+
+```
+C:\code\info498e\lab04-composites>node composite-html-2.js
+<html>
+<div>
+<p>
+Hello world!
+</p>
+<p>
+How is it going?
+</p>
+</div>
+</html>
+
+```
+
+We have given you some starter code that includes an interface (IDomElement). You need to create a class (DomElement) that implements this interface.
+
+```javascript
+interface IDomElement {
+  add(element: DomElement);
+  print();
+}
+```
+
+Each instance of this DomElement class will represent a element (you can also call them nodes) in your DOM tree.
+
+Use this composite structure to create a DOM tree that looks like the sample output we've provided you above.
--- composite-html-2.ts
+++ composite-html-2.ts
@@ -1,42 +1,46 @@
+# Composites Lab
+
+## What is a composite?
+
+Put high level conceptual introduction to what a composite is.
+Give 1-2 links to really good explanations out there by other people.
+
+OR
+
+delete this section and give a mini lecture in person.
+
+## Your task
+
+include some HTML DOM review content here
+
+Your task for this lab is to write TypeScript that constructs and prints out an HTML DOM using the composites pattern.
+
+Your output will look something like this:
+
+```
+C:\code\info498e\lab04-composites>node composite-html-2.js
+<html>
+<div>
+<p>
+Hello world!
+</p>
+<p>
+How is it going?
+</p>
+</div>
+</html>
+
+```
+
+We have given you some starter code that includes an interface (IDomElement). You need to create a class (DomElement) that implements this interface.
+
+```javascript
 interface IDomElement {
   add(element: DomElement);
   print();
 }
+```
 
-class DomElement implements IDomElement {
-  elementName : string;
-  textContent: string;
-  elements: DomElement[];
+Each instance of this DomElement class will represent a element (you can also call them nodes) in your DOM tree.
 
-  constructor(elementName : string, textContent?: string) {
-    this.elementName = elementName;
-    this.elements = [];
-    this.textContent = textContent;
-  }
-
-  add(element: DomElement) {
-    this.elements.push(element);
-  }
-
-  print() {
-    console.log(`<${this.elementName}>`);
-    if (this.textContent) {
-      console.log(this.textContent);
-    }
-    for (let element of this.elements) {
-      element.print();
-    }
-    console.log(`</${this.elementName}>`);
-  }
-}
-
-let p1 = new DomElement("p", "Hello world!");
-let p2 = new DomElement("p", "How is it going?");
-let div = new DomElement("div");
-let html = new DomElement("html");
-
-div.add(p1);
-div.add(p2);
-html.add(div);
-
-html.print();
+Use this composite structure to create a DOM tree that looks like the sample output we've provided you above.
